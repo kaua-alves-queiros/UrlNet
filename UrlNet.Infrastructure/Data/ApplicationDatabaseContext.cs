4e18c9c5640c15e7285759fbdd3384897f20fe56
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-10-03 01:07:47 -0400

    adicionado agreagation aos models do context

--- UrlNet.Infrastructure/Data/ApplicationDatabaseContext.cs
+++ UrlNet.Infrastructure/Data/ApplicationDatabaseContext.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UrlNet.Domain.Aggregations;
 
 namespace UrlNet.Infrastructure.Data
 {
     public class ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options) : DbContext (options)
     {
+        public DbSet<ShortenedUrl> ShortenedUrls => default!;
     }
 }
